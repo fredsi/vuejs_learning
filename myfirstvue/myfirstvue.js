fb98e914fd3a824e0b29843713c1ced523a197d5
fred.si <fredsi@augmentum.com.cn>
2019-01-11 16:24:29 +0800

    add-methods-test

--- myfirstvue/myfirstvue.js
+++ myfirstvue/myfirstvue.js
@@ -5,4 +5,32 @@ var app = new Vue({
     }
 })
 
-app.message = 'I have changed the data!';
+app.message = 'I have changed the data!'
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: '页面加载于 ' + new Date().toLocaleTimeString()
+    }
+})
+
+var app3 = new Vue({
+    el:'#app-3',
+    data:{
+        seen: true
+    }
+})
+app3.seen = false
+
+
+var app4 = new Vue({
+    el:'#app-4',
+    data:{
+        todos: [
+            {text: "Learn Js"},
+            {text: "Learn Vue"},
+            {text: "Whole project"}
+        ]
+    }
+})
+app4.todos.push({ text: '新项目' })
